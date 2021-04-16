5d4bd06e3631d646aeb3119b349f640229751f6c
hosp-nckm <76767127+hosp-nckm@users.noreply.github.com>
2021-04-16 10:56:42 +0800

    PWA

---
+++ web/粉紅旅程/sw.js
@@ -0,0 +1,17 @@
+// 引用workbox build
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+// 使用precache功能，在offline下也可以執行
+// 要存進cache storage裡的檔案清單
+var cacheFiles = [
+  "sw.js",
+  /*"assets/bootstrap.min.css",
+  "assets/bootstrap.min.js",
+  "assets/jquery-3.3.1.slim.min.js",
+  "assets/test.jpg",*/
+  {
+    url: './PWA.html',
+    revision: '00000001' // 加revision，版本改了以後，sw.js 在 application 上會更新
+  }
+];
+workbox.precaching.precacheAndRoute(cacheFiles);
