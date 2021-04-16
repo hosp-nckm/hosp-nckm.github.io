620fea86b7ec1691114caaac57fe338be03d3dea
hosp-nckm <76767127+hosp-nckm@users.noreply.github.com>
2021-04-16 11:11:05 +0800

    Update sw.js

--- web/粉紅旅程/sw.js
+++ web/粉紅旅程/sw.js
@@ -4,7 +4,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 // 使用precache功能，在offline下也可以執行
 // 要存進cache storage裡的檔案清單
 var cacheFiles = [
-  "sw.js",
+  "static/video/乳房重建衛教影片 國語版 實驗組 最新版_1.mp4",
   /*"assets/bootstrap.min.css",
   "assets/bootstrap.min.js",
   "assets/jquery-3.3.1.slim.min.js",
