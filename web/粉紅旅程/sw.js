4571be97e4d27246f7a78b581223b2445773d4d0
hosp-nckm <76767127+hosp-nckm@users.noreply.github.com>
2021-04-16 13:00:12 +0800

    Update sw.js

--- web/粉紅旅程/sw.js
+++ web/粉紅旅程/sw.js
@@ -5,12 +5,13 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 // 要存進cache storage裡的檔案清單
 var cacheFiles = [
   "./static/video/乳房重建衛教影片 國語版 實驗組 最新版_1.mp4",
+  "static/img/logo.PNG",
   /*"assets/bootstrap.min.css",
   "assets/bootstrap.min.js",
   "assets/jquery-3.3.1.slim.min.js",
   "assets/test.jpg",*/
   {
-    url: './PWA.html',
+    url: './home.html',
     revision: '00000001' // 加revision，版本改了以後，sw.js 在 application 上會更新
   }
 ];
