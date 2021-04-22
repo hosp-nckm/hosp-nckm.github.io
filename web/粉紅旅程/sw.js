c0ac9e714edd6fd9c76e32340ee7debed84cd479
hosp-nckm <76767127+hosp-nckm@users.noreply.github.com>
2021-04-22 15:11:10 +0800

    new PWA

--- web/粉紅旅程/sw.js
+++ web/粉紅旅程/sw.js
@@ -1,18 +1,10 @@
-// 引用workbox build
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
-
-// 使用precache功能，在offline下也可以執行
-// 要存進cache storage裡的檔案清單
-var cacheFiles = [
-  "./static/video/乳房重建衛教影片 國語版 實驗組 最新版_1.mp4",
-  "static/img/logo.PNG",
-  /*"assets/bootstrap.min.css",
-  "assets/bootstrap.min.js",
-  "assets/jquery-3.3.1.slim.min.js",
-  "assets/test.jpg",*/
-  {
-    url: './home.html',
-    revision: '00000001' // 加revision，版本改了以後，sw.js 在 application 上會更新
-  }
-];
-workbox.precaching.precacheAndRoute(cacheFiles);
+new workboxPlugin.InjectManifest({
+    swSrc: path.join(__dirname, '..', SRC_DIR, 'sw.js'),
+    swDest: path.join(__dirname, '..', DIST_DIR, 'sw.js'),
+    globDirectory: path.join(__dirname, '..', DIST_DIR),
+    globPatterns: ['**/*.{js,css}']
+}),
+  
+//sw.js
+let precacheList = self.__precacheManifest || []
+workbox.precaching.precacheAndRoute(precacheList)
