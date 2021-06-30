8c9fdfca7d61a68e4fc555312fbb7cc289c83496
hosp-nckm <76767127+hosp-nckm@users.noreply.github.com>
2021-06-30 18:54:46 +0800

    fetch home_PWA

--- web/粉紅旅程/sw_home.js
+++ web/粉紅旅程/sw_home.js
@@ -44,6 +44,6 @@ self.addEventListener('fetch', event => {
   if (event.request.mode === 'navigate') {
     // Always respond to navigations with the cached app-shell.html,
     // regardless of the underlying event.request.url value.
-    event.respondWith(caches.match('app-shell.html'));
+    event.respondWith(caches.match('home_PWA.html'));
   }
 });
