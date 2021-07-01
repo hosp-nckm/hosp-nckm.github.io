b56ca064caa618d59e535376ea3c2b9cfe76a6c4
hosp-nckm <76767127+hosp-nckm@users.noreply.github.com>
2021-07-02 01:17:34 +0800

    加/   去 "/",

--- web/粉紅旅程/sw_home.js
+++ web/粉紅旅程/sw_home.js
@@ -4,7 +4,6 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 // 使用precache功能，在offline下也可以執行
 // 要存進cache storage裡的檔案清單
 var cacheFiles = [
-  "/",
   "static/img/favicon.ico",
   "static/img/logo.PNG",
   "static/img/icon_192.png",
