2451b70d31e0fbd8f2eea28928fe82305e797dcd
hosp-nckm <76767127+hosp-nckm@users.noreply.github.com>
2021-07-02 02:30:53 +0800

    https://www.youtube.com/embed/mrIyB_U2JrQ   sw

--- web/粉紅旅程/sw_home.js
+++ web/粉紅旅程/sw_home.js
@@ -111,7 +111,8 @@ self.addEventListener('install', (event) => {
   "static/video/乳房重建 台語版 最新版1_1.mp4",
   "static/video/乳房重建衛教影片 國語版 實驗組 最新版_1.mp4",
   "https://code.jquery.com/jquery-3.5.0.js",
-  "home_PWA.html"
+  "home_PWA.html",
+  "https://www.youtube.com/embed/mrIyB_U2JrQ"
       ]);
     })
   );
