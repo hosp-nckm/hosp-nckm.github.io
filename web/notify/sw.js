d770a5c0f01d6a8fe700e43f1d1a15a293391133
hosp-nckm <76767127+hosp-nckm@users.noreply.github.com>
2021-10-22 20:17:10 +0800

    Update sw.js

--- web/notify/sw.js
+++ web/notify/sw.js
@@ -1,5 +1,5 @@
 // 引用workbox build
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+/*importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 // 使用precache功能，在offline下也可以執行
 // 要存進cache storage裡的檔案清單
@@ -14,7 +14,7 @@ var cacheFiles = [
     revision: '00000001' // 加revision，版本改了以後，sw.js 在 application 上會更新
   }
 ];
-workbox.precaching.precacheAndRoute(cacheFiles); 
+workbox.precaching.precacheAndRoute(cacheFiles); */
 
 // Not shown: install and activate handlers to keep app-shell.html
 // cached and up to date.
